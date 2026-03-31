022dee64680d61b9ab11078562e5f3df68163023
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-03-31 11:27:39 -0700

    Updated main_read.cpp

--- main_read.cpp
+++ main_read.cpp
@@ -11,16 +11,30 @@ int main()
     ifstream ifs;
 
     // TODO: open "students.txt" for reading
+    ifs.open("Students.txt");
 
     // TODO: check if file opened successfully; print error and exit if not
+    if (!ifs.is_open()){
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
 
     // TODO: read M from ifs, print "Total M students"
+    ifs >> M;
 
     for (int i = 0; i < M; i++)
     {
         // TODO: read stuName, score1, score2 from ifs
+        ifs >> stuName >> score1 >> score2;
+
         // TODO: compute sum = score1 + score2, avg = sum / 2.0
+        sum = score1 + score2;
+        avg = sum / 2.0;
+        
         // TODO: print Student Name, score1, score2, Sum, Avg
+        cout << stuName << " " << score1 << " " << score2
+            << " " << sum << " " << avg << endl;
+    
     }
     ifs.close();
     return 0;
